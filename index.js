3740ccc68b871637fb375e8277943e67ac89a35f
Daniel Maldonado <dmaldo01@calpoly.edu>
2017-11-12 18:07:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(drivers,name){
+
+  let MatchingDrivers = drivers.filter(function(driver){
+    if(name.toLowerCase() ===driver.toLowerCase()){
+      return true
+    } else {
+      return false
+    }
+    }
+  })
+  return MatchingDrivers;
+}
